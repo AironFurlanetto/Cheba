e3e14fea5fa9363619a08a66d46c918ebba6c2dd
Airon Batista Furlanetto <107115298+AironFurlanetto@users.noreply.github.com>
2023-11-26 16:59:17 -0300

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,8 @@
+// function showcontent(id){
+//     var conteudo = document.getElementsByClassName('conteudo');
+//     for (var i = 0; i < contents.length; i++) {
+//         contents[i].classList.remove('active');
+//       }
+//     var selectedContent = document.getElementById(id);
+//     selectedContent.classList.add('active');
+//     }
