35329fe375d2dbfe0268ce311ec80f3926cbfb41
Airon Batista Furlanetto <107115298+AironFurlanetto@users.noreply.github.com>
2023-11-28 22:44:07 -0300

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,8 +1,12 @@
-// function showcontent(id){
-//     var conteudo = document.getElementsByClassName('conteudo');
-//     for (var i = 0; i < contents.length; i++) {
-//         contents[i].classList.remove('active');
-//       }
-//     var selectedContent = document.getElementById(id);
-//     selectedContent.classList.add('active');
-//     }
+<script>
+                const radioButtons = document.querySelectorAll('.radio-buttons input');
+                const slides = document.querySelectorAll('.slide');
+            
+                radioButtons.forEach((radio, index) => {
+                  radio.addEventListener('change', () => {
+                    slides.forEach((slide, slideIndex) => {
+                      slide.style.transform = `translateX(${(slideIndex - index) * 100}%)`;
+                    });
+                  })
+                })
+              </script>
