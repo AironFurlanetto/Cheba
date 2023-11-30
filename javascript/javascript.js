8ce9061f7699dbb3d14314421df5418ea1ebe3a4
Airon Batista Furlanetto <107115298+AironFurlanetto@users.noreply.github.com>
2023-11-29 22:48:30 -0300

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,12 +1,18 @@
-<script>
-                const radioButtons = document.querySelectorAll('.radio-buttons input');
-                const slides = document.querySelectorAll('.slide');
-            
-                radioButtons.forEach((radio, index) => {
-                  radio.addEventListener('change', () => {
-                    slides.forEach((slide, slideIndex) => {
-                      slide.style.transform = `translateX(${(slideIndex - index) * 100}%)`;
-                    });
-                  })
-                })
-              </script>
+let currentIndex = 0;
+  const items = document.querySelectorAll('.carrossel');
+  const totalItems = items.length;
+
+  function slideprox() {
+    items[currentIndex].style.display = 'none';
+    currentIndex = (currentIndex + 1) % totalItems;
+    items[currentIndex].style.display = 'block';
+  }
+
+  function Slidevolta() {
+    items[currentIndex].style.display = 'none';
+    currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+    items[currentIndex].style.display = 'block';
+  }
+  for (let i = 1; i < totalItems; i++) {
+    items[i].style.display = 'none';
+  }
