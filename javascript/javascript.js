e08ff3180407c14a68a780656bd898279e73951b
Airon Batista Furlanetto <107115298+AironFurlanetto@users.noreply.github.com>
2023-11-30 11:43:15 -0300

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,18 +1,19 @@
-let currentIndex = 0;
-  const items = document.querySelectorAll('.carrossel');
-  const totalItems = items.length;
+let Index = 0; // declaraçao de variaveis
+  const item = document.querySelectorAll('.carrossel');
+  const total = item.length;
 
-  function slideprox() {
-    items[currentIndex].style.display = 'none';
-    currentIndex = (currentIndex + 1) % totalItems;
-    items[currentIndex].style.display = 'block';
+  function slideprox() { // mexe os slides do carrossel pra frente
+    item[Index].style.display = 'none';
+    Index = (Index + 1) % total;
+    item[Index].style.display = 'block';
   }
 
-  function Slidevolta() {
-    items[currentIndex].style.display = 'none';
-    currentIndex = (currentIndex - 1 + totalItems) % totalItems;
-    items[currentIndex].style.display = 'block';
+  function Slidevolta() { // mexe os slides do carrossel pra tras
+    item[Index].style.display = 'none';
+    Index = (Index - 1 + total) % total;
+    item[Index].style.display = 'block';
   }
-  for (let i = 1; i < totalItems; i++) {
-    items[i].style.display = 'none';
+
+  for (let i = 1; i < total; i++) { // faz com que o site inicie apenas com a primeira image do carrossel
+    item[i].style.display = 'none';
   }
